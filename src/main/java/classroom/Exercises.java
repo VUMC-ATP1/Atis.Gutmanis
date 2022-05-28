57f5ac3f1092be9d8ea83e1843d0196be951064f
AtisG <gutmanis.atis@gmail.com>
2022-05-28 14:56:09 +0300

    [feature/java]: Homework progress add

---
+++ src/main/java/classroom/Exercises.java
@@ -0,0 +1,38 @@
+package classroom;
+
+import java.util.Scanner;
+
+public class Exercises {
+    public static void main(String[] args) {
+
+
+
+
+      /*  Scanner videji = new Scanner(System.in);
+        int a, b, c;
+        System.out.println("Ievadi pirmo skaitli!");
+        a = videji.nextInt();
+        System.out.println("Ievadi otro skaitli!");
+        b = videji.nextInt();
+        System.out.println("Ievadi trešo skaitli");
+        c = videji.nextInt();
+        System.out.println("Šo skaitļu aritmētiski vidējais ir " + ((a + b + c) / 3) + " !");
+*/
+
+
+
+        /*Scanner skeneris = new Scanner(System.in);
+        int a;
+        System.out.println("Ievadi skaitli!");
+        a = skeneris.nextInt();
+        if (a < 0){
+            System.out.println("Skaitlis mazāks par 0!");
+        } else if (a == 0) {
+                System.out.println("Skaitlis ir 0!");
+            } else System.out.println("Skaitlis lielāks par nulli");
+        }
+
+    }*/
+
+    }
+    }
