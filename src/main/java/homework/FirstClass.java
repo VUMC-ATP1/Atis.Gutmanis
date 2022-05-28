7b1d0125cccfff42aadf72a805a0216ce9dfd877
AtisG <gutmanis.atis@gmail.com>
2022-05-28 17:00:38 +0300

    [feature/java]: Homework progress add

--- src/main/java/homework/FirstClass.java
+++ src/main/java/homework/FirstClass.java
@@ -3,21 +3,21 @@ package homework;
 public class FirstClass {
     public static void main(String[] args) {
 
-        String Country = "Djibouti";
-        int Population = 921804;
-        float Area = 37.2f;
-        String Capital = "Djibouti";
-        String OfficialLanguage = "Arabic";
+        String country = "Djibouti";
+        int population = 921804;
+        float area = 37.2f;
+        String capital = "Djibouti";
+        String officialLanguage = "Arabic";
         boolean isMember = false;
         String result = "";
         if (isMember == false) {
             result = " NOT";
         }
 
-        String Currency = "Fdj";
-        char Demand = '\u1200';
+        String currency = "Fdj";
+        char demand = '\u1200';
 
-        System.out.println("The topic of the conversation is going to be " + Country +". It has a population of " + Population + " that are all squeezed in " + Area + " square kilometers. The capital is " + Capital + " and people there speak " + OfficialLanguage + ". The symbol for their currency is " + Currency + " but this test wants me to write a char, not a string, so I will just add a random symbol. I don't even know what it will be. Here we go - " + Demand + ". There. Best Symbol in the world. And the country is"  + result + " a member of European Union." );
+        System.out.println("The topic of the conversation is going to be " + country +". It has a population of " + population + " that are all squeezed in " + area + " square kilometers. The capital is " + capital + " and people there speak " + officialLanguage + ". The symbol for their currency is " + currency + " but this test wants me to write a char, not a string, so I will just add a random symbol. I don't even know what it will be. Here we go - " + demand + ". There. Best Symbol in the world. And the country is"  + result + " a member of European Union." );
 
         int g = 100;
         int o = 11;
